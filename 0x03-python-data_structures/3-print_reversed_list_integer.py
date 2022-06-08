6c02e27c8fb4c77778bac797a1440f3c863220ab
Munene <muneneemasonmutuma@gmail.com>
2022-06-08 12:21:10 +0300

    account for list is None

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
+    if my_list is None:
+        return
     i = len(my_list)
     while i:
         print("{:d}".format(my_list[i - 1]))
