911b062d4bdf4be5eb90718bc35832f74d34c8c8
Munene <muneneemasonmutuma@gmail.com>
2022-06-08 12:32:28 +0300

    account for empty sentence

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,5 +2,8 @@
 
 def multiple_returns(sentence):
     length = len(sentence)
-    first = sentence[0] or None
+    if length == 0:
+        first = None
+    else:
+        first = sentence[0]
     return length, first
