119ab30ed7e09db3aaa828069a20afd28aff1e78
Munene <muneneemasonmutuma@gmail.com>
2022-06-21 15:00:59 +0300

    1.square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""A module that describes Square class with size attribute
+"""
+
+
+class Square:
+    """This is an empty class for `Square`
+    """
+
+    def __init__(self, size):
+        """Initializer method for the class `Square`.
+
+        Args:
+            size: size of the square
+        """
+        self.__size = size
