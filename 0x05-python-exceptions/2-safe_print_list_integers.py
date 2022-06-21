78a7edc819b094d11876019964b91cd644d0310d
Munene <muneneemasonmutuma@gmail.com>
2022-06-21 10:42:45 +0300

    2. allow IndexError

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,11 +4,7 @@ def safe_print_list_integers(my_list=[], x=0):
     i = 0
     count = 0
     while (i < x):
-        try:
-            val = my_list[i]
-        except IndexError:
-            print("")
-            return count
+        val = my_list[i]
 
         try:
             print("{:d}".format(val), end="")
