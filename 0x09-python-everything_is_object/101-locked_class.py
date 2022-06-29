4513631cc06191fac34a9494da0751e3b19f7a1b
Munene Mutuma <muneneemasonmutuma@gmail.com>
2022-06-29 20:57:35 +0300

    docs

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
+"""Module that implements a simple LockedClass
+"""
+
 
 class LockedClass:
+    """Class that allows only an attribute called "first_name"
+    """
     def __setattr__(self, attribute, value):
+        """override superclass method __setattr__
+        """
         if attribute != "first_name":
             raise AttributeError("'{}' object has no attribute '{}'".format(
                                     self.__class__.__name__, attribute))
