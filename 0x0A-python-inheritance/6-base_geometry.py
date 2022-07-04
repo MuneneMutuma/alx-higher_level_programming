9d4bc9df92116f8e3e8d5c5f430dccffe21f46fe
Munene Mutuma <muneneemasonmutuma@gmail.com>
2022-07-04 22:37:18 +0300

    6.

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""A module that creates an empty class BasicGeometry
+"""
+
+
+class BaseGeometry:
+    """Class that implements basic area method
+    """
+    def area(self):
+        """method that raises Exception when called
+        """
+        raise Exception("area() is not implemented")
