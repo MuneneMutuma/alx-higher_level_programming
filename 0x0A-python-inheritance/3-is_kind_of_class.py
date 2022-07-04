aaffb9c722f731d044eb83af0bf4f9d358e7256f
Munene Mutuma <muneneemasonmutuma@gmail.com>
2022-07-04 22:14:40 +0300

    3.1 docs

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
+"""A Module that checks if an object is type of a class
+"""
 
 
 def is_kind_of_class(obj, a_class):
+    """function that checks whether an object is of type class
+
+    Attributes:
+        obj (obj): object to be checked
+        a_class (type): type or class to be checked against
+
+    Return:
+        True: if obj is an instance of a_class, else False
+    """
+
     if isinstance(obj, a_class):
         return True
     else:
