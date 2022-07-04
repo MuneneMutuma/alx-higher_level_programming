65542301f0c5d89f9ab9600f175b1fedf69d7806
Munene Mutuma <muneneemasonmutuma@gmail.com>
2022-07-04 21:32:48 +0300

    pycodestyle

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
 
+"""Module that provides method to look up list of attributes and methods
+"""
+
+    
 def lookup(obj):
+    """looks up the attributes and methods of class
+
+    Attr:
+        obj: object
+    """
     return (dir(obj))
