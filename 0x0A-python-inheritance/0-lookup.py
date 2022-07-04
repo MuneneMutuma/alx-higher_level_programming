d1799e5488ffc168b3d14ff25e6d98220d792c81
Munene Mutuma <muneneemasonmutuma@gmail.com>
2022-07-04 21:37:52 +0300

    add tests folder

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-
 """Module that provides method to look up list of attributes and methods
 """
 
-    
+
 def lookup(obj):
     """looks up the attributes and methods of class
 
