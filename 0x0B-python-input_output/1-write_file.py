39c5af5da86415e9d54079bc965c4a2eae9d8b66
Munene Mutuma <muneneemasonmutuma@gmail.com>
2022-07-05 21:44:43 +0300

    docs

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
+"""A module that writes to a file
+"""
 
 
 def write_file(filename="", text=""):
+    """A function that writes to a file
+
+    Attributes:
+        filename (str): filename to be opened
+        text (str): text to be written to file
+
+    Returns:
+        chars (int): number of characters written
+    """
     with open(filename, 'w', encoding="utf-8") as f:
         chars = f.write(text)
 
