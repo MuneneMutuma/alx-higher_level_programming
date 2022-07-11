27f70b7d9493676f14dbc4bdb6cfe2ef047337af
Munene Mutuma <muneneemasonmutuma@gmail.com>
2022-07-11 23:22:02 +0300

    15. make to_json_string a static method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -14,7 +14,7 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-
+    @staticmethod
     def to_json_string(list_dictionaries):
         """returns the JSON string representation of `list_dictionary`"""
         if list_dictionaries is None or len(list_dictionaries) == 0:
