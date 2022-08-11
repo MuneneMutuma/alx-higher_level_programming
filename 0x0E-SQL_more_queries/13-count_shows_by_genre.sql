cb0418e36b581ded401080146c4356c0a8a49cfc
Munene Mutuma <muneneemasonmutuma@gmail.com>
2022-08-11 13:29:06 +0300

    all

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,2 @@
+-- a script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
+SELECT tv_genres.name genre, COUNT(*) number_of_shows FROM tv_genres JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id GROUP BY  genre ORDER BY number_of_shows DESC;
