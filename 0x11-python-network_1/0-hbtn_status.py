ffefd2d9d84f5c575068f167bc6666c4a2223dae
Munene Mutuma <muneneemasonmutuma@gmail.com>
2022-09-23 12:49:29 +0300

    hbtn status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import urllib.request
+
+req = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+
+with urllib.request.urlopen(req) as response:
+    page = response.read()
+    print("Body response:")
+    print(f"\t- type: {type(page)}")
+    print(f"\t- content: {page}")
+    print(f"\t- type: {page.decode('utf-8')}")
