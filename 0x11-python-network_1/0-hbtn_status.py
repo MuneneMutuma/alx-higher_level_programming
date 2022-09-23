de4950750af6fcdc5fa6192112ddd4db37ccf90f
Munene Mutuma <muneneemasonmutuma@gmail.com>
2022-09-23 16:30:42 +0300

    0.3

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
+"""Fetches https://alx-intranet.hbtn.io/stats"""
+from urllib import request
 
-import urllib.request
+if __name__ == "__main__":
+    req = request.Request("https://alx-intranet.hbtn.io/status")
 
-req = urllib.request.Request("https://alx-intranet.hbtn.io/status")
-
-with urllib.request.urlopen(req) as response:
-    page = response.read()
-    print("Body response:")
-    print(f"\t- type: {type(page)}")
-    print(f"\t- content: {page}")
-    print(f"\t- utf-8 content: {page.decode('utf-8')}")
+    with request.urlopen(req) as response:
+        page = response.read()
+        print("Body response:")
+        print(f"\t- type: {type(page)}")
+        print(f"\t- content: {page}")
+        print(f"\t- utf-8 content: {page.decode('utf-8')}")
