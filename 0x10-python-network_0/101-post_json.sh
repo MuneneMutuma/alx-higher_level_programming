00797cb5721f8a853914cd06f67eded4e9b95fe0
Munene Mutuma <muneneemasonmutuma@gmail.com>
2022-09-23 12:20:18 +0300

    101.2

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a Bash script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
-curl -sfL -X POST -d @"$2" "$1"
+curl -sfL -X POST -d @"$2" --header "Content-Type: application/json" "$1"
