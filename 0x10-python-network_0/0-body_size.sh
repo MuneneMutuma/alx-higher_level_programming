bd8131695132aaf3f1ab1d0c8a3dc15d426a170f
Munene Mutuma <muneneemasonmutuma@gmail.com>
2022-09-23 09:25:10 +0300

    correct shebang

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 #takes in URL sends a request to that url, and displays the size of the body of the response
 
 curl -so /dev/null 0.0.0.0:5000 -w '%{size_download}\n'
