acc87cd08e82ac8ed2acedfa9095f1abe0a96e26
Munene Mutuma <muneneemasonmutuma@gmail.com>
2022-09-23 09:34:18 +0300

    use parameters

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #takes in URL sends a request to that url, and displays the size of the body of the response
-curl -so /dev/null 0.0.0.0:5000 -w '%{size_download}\n'
+curl -so /dev/null "$1" -w '%{size_download}\n'
