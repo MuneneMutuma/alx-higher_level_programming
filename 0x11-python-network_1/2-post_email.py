88a2913f274f95e5e394f2921b6aece6350c5f5c
Munene Mutuma <muneneemasonmutuma@gmail.com>
2022-09-27 09:51:16 +0300

    2.1

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """sends post request and prints response body"""
 
-import urllib.request as request
-import urllib.parse as parse
+import urllib.request
+import urllib.parse
 import sys
 
 if __name__ == "__main__":
-    req = request.Request(sys.argv[1])
-    data = parse.urlencode({"email": f"{sys.argv[2]}"}).encode()
-    with request.urlopen(req, data=data) as response:
-        print(response.read().decode("utf-8"))
+    req = urllib.request.Request(sys.argv[1])
+    data = urllib.parse.urlencode({"email": f"{sys.argv[2]}"}).encode()
+    with urllib.request.urlopen(req, data=data) as response:
+        print(urllib.response.read().decode("utf-8"))
