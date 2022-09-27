4fc8f83f054f6e68ec4eec4852f363456ef9f6a6
Munene Mutuma <muneneemasonmutuma@gmail.com>
2022-09-27 16:30:15 +0300

    9, 8.1 semistandard

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,13 +2,12 @@
 
 const size = parseInt(process.argv[2]);
 
-if (isNaN(size))
-{
+if (isNaN(size)) {
   console.log('Missing size');
 } else {
-  let str = [];
+  const str = [];
   for (let y = 0; y < size; y++) {
-    str.push('X')
+    str.push('X');
   }
   for (let x = 0; x < size; x++) {
     console.log(str.join(''));
