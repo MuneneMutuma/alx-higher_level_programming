69fb4443e24fa8dc648deb2068e1c3c3b2d263c6
Munene Mutuma <muneneemasonmutuma@gmail.com>
2022-09-27 09:43:40 +0300

    module docs

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""a Python script that takes in a URL, sends a request
-to the URL and displays the value of the X-Request-Id
-variable found in the header of the response."""
+"""sends a request to url given and prints header parameter X-Request-Id"""
 
 import urllib.request
 import sys
