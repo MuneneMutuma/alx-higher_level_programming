7934e67a56585a7147c05e915be53ab71f95a367
Munene Mutuma <muneneemasonmutuma@gmail.com>
2022-09-27 16:27:02 +0300

    8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const size = parseInt(process.argv[2]);
+
+if (isNaN(size))
+{
+  console.log('Missing size');
+} else {
+  let str = [];
+  for (let y = 0; y < size; y++) {
+    str.push('X')
+  }
+  for (let x = 0; x < size; x++) {
+    console.log(str.join(''));
+  }
+}
