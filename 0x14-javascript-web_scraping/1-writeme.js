63d3aa2190a966c1ade01faacb626a2e0249bafa
Munene Mutuma <muneneemasonmutuma@gmail.com>
2022-10-18 15:52:29 +0300

    1. correct error handling

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,4 +4,6 @@ const fs = require('fs');
 
 const file = process.argv[2];
 const data = process.argv[3];
-fs.writeFile(file, data, 'utf-8', (err) => console.log(err));
+fs.writeFile(file, data, 'utf-8', (err) => {
+  if (err) console.log(err);
+});
