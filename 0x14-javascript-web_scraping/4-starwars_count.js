fa8d67d1f7c906fb667edce458a668bbd677d5da
Munene Mutuma <muneneemasonmutuma@gmail.com>
2022-10-18 14:56:53 +0300

    handle only status 200, use include to test 18

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,15 +3,17 @@
 const request = require('request');
 const url = process.argv[2];
 request(url, (error, response, body) => {
-	let results = JSON.parse(body).results;
-	let count = 0;
-	const link = "https://swapi-api.hbtn.io/api/people/18/";
-
-	for (let i = 0; i < results.length; i++) {
-		let characters = results[i].characters;
-		for (let x = 0; x < characters.length; x++) {
-			if (characters[x] === link) count++;
-		}
-	}
-	console.log(count);
-})
+  const results = JSON.parse(body).results;
+  let count = 0;
+  if (error) {
+    throw error;
+  } else if (response.statusCode === 200) {
+    for (let i = 0; i < results.length; i++) {
+      const characters = results[i].characters;
+      for (let x = 0; x < characters.length; x++) {
+        if (characters[x].includes('18')) count++;
+      }
+    }
+    console.log(count);
+  }
+});
